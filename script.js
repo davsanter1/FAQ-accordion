ffec04e413a74eef79e550bbaa6cbc4a2daf9d7c
davsanter1 <141477110+davsanter1@users.noreply.github.com>
2024-01-28 21:57:56 -0600

    completed but faulty :')

--- script.js
+++ script.js
@@ -13,3 +13,59 @@ function show(){
 qHeader.addEventListener('click',show)
 iMinus.addEventListener('click',show)
 iPlus.addEventListener('click',show)
+parag.addEventListener('click',show)
+
+//for question 2 -----------------------------------------------------------------------------------------------------------
+
+let qHeader2 = document.getElementById ('header2')
+let iMinus2 = document.getElementById ('minus2')
+let iPlus2 = document.getElementById ('plus2')
+let parag2 = document.getElementById ('prg2')
+
+function show2(){
+    parag2.style.display === 'none' ? parag2.style.display = 'block': parag2.style.display ='none';
+    iPlus2.style.display === 'block' ? iPlus2.style.display = 'none': iPlus2.style.display ='block';
+    iMinus2.style.display === 'none' ? iMinus2.style.display = 'block': iMinus2.style.display ='none';
+
+}
+
+qHeader2.addEventListener('click',show2)
+iMinus2.addEventListener('click',show2)
+iPlus2.addEventListener('click',show2)
+parag2.addEventListener('click',show2)
+
+//for question 3 -----------------------------------------------------------------------------------------------------------
+let qHeader3 = document.getElementById ('header3')
+let iMinus3 = document.getElementById ('minus3')
+let iPlus3 = document.getElementById ('plus3')
+let parag3 = document.getElementById ('prg3')
+
+function show3(){
+    parag3.style.display === 'none' ? parag3.style.display = 'block': parag3.style.display ='none';
+    iPlus3.style.display === 'block' ? iPlus3.style.display = 'none': iPlus3.style.display ='block';
+    iMinus3.style.display === 'none' ? iMinus3.style.display = 'block': iMinus3.style.display ='none';
+
+}
+
+qHeader3.addEventListener('click',show3)
+iMinus3.addEventListener('click',show3)
+iPlus3.addEventListener('click',show3)
+parag3.addEventListener('click',show3)
+
+//for question 4 -----------------------------------------------------------------------------------------------------------
+let qHeader4 = document.getElementById ('header4')
+let iMinus4 = document.getElementById ('minus4')
+let iPlus4 = document.getElementById ('plus4')
+let parag4 = document.getElementById ('prg4')
+
+function show4(){
+    parag4.style.display === 'none' ? parag4.style.display = 'block': parag4.style.display ='none';
+    iPlus4.style.display === 'block' ? iPlus4.style.display = 'none': iPlus4.style.display ='block';
+    iMinus4.style.display === 'none' ? iMinus4.style.display = 'block': iMinus4.style.display ='none';
+
+}
+
+qHeader4.addEventListener('click',show4)
+iMinus4.addEventListener('click',show4)
+iPlus4.addEventListener('click',show4)
+parag4.addEventListener('click',show4)
